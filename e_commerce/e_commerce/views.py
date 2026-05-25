8093007aa1e20b69f24d099a668024bc2d8f88ea
Ganesh369-programmer <ganesh3692005@gmail.com>
2026-05-25 10:24:21 +0530

    Stock related task

--- e_commerce/e_commerce/views.py
+++ e_commerce/e_commerce/views.py
@@ -5,8 +5,10 @@ from store.models import Product
 def home(request):
     products = Product.objects.all().filter(is_available=True)
 
+   
     context = {
         'products' : products , 
+        
 
     }
 
